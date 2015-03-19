2e5a323b6f56817329c725bf2c779f0122d4b57a
c-acoronel <acoronel@hxpws-acoronel.hexacta.com>
2015-03-19 11:11:58 -0300

    Updating grunt-wiredep to 1.9.0 to avoid warning while running grunt serve command.

--- Gruntfile.js
+++ Gruntfile.js
@@ -163,7 +163,7 @@ module.exports = function (grunt) {
     // Automatically inject Bower components into the app
     wiredep: {
       options: {
-        cwd: '<%= yeoman.app %>'
+        // cwd: '<%= yeoman.app %>' // Commented to avoid warning while running grunt serve command
       },
       app: {
         src: ['<%= yeoman.app %>/index.html'],
